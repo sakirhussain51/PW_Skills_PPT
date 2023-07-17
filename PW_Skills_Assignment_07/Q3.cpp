9e90e0ddb71231217653080a6724571c7f7d5ecb
Sakir Shah <44570011+sakirhussain51@users.noreply.github.com>
2023-07-17 22:38:06 +0530

    Update Q3.cpp

--- PW_Skills_Assignment_07/Q3.cpp
+++ PW_Skills_Assignment_07/Q3.cpp
@@ -1,1 +1,27 @@
 
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    string str,str1;
+    cin>>str>>str1;
+    int n=str.length();
+    int n1=str1.length();
+    int num=0;
+    for(int i=0;i<n;i++){
+        if(str[i]>='0' && str[i] <='9'){
+            num=num*10+str[i]-'0';
+        }
+    }
+    int num2=0;
+    for(int i=0;i<n1;i++){
+    if(str1[i]>='0' && str1[i]<='9'){
+            num2=num2*10+str1[i]-'0';
+        }
+    }
+    long long int sum=num+num2;
+    cout<<sum<<endl;
+return 0;
+}
