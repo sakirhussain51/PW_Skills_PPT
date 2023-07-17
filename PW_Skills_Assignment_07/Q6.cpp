93e85ce67f4b3bc3278a93a32a881176261d9d7c
Sakir Shah <44570011+sakirhussain51@users.noreply.github.com>
2023-07-17 22:39:26 +0530

    Update Q6.cpp

--- PW_Skills_Assignment_07/Q6.cpp
+++ PW_Skills_Assignment_07/Q6.cpp
@@ -1,1 +1,34 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+int main()
+{
+
+    cout << "Enter the Strig?" << endl;
+    string str;
+    cin >> str;
+
+    int len = str.length();
+    cout << "Enter the goal string" << endl;
+    string goal;
+    cin >> goal;
+    int goal_len = str.length();
+
+    string concetinate = str + str;
+
+    cout << "Result after some shift of character:" << endl;
+
+    if (goal_len != len)
+        cout << "false" << endl;
+    else if (concetinate.find(goal) != string::npos)
+        cout << "true";
+    else
+    {
+        cout << "false";
+    }
+
+    cout << endl;
+
+    return 0;
+}
