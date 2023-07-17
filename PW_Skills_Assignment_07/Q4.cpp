cd89636d72cbfe1d0c22d7741408315b4e19d3cb
Sakir Shah <44570011+sakirhussain51@users.noreply.github.com>
2023-07-17 22:38:34 +0530

    Update Q4.cpp

--- PW_Skills_Assignment_07/Q4.cpp
+++ PW_Skills_Assignment_07/Q4.cpp
@@ -1,1 +1,31 @@
 
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    string str;
+    cout<<"Enter the Strig"<<endl;
+    getline(cin,str);
+
+    int len = str.length();
+    int left =0;
+    int right=0;
+  
+     while(left<len)
+     {
+        while((str[right]!=' ') && right<len){
+            right++;
+        }
+
+           reverse(str.begin()+left,str.begin()+right);
+            left=right+1;
+            right=left;
+
+     }
+         cout<<"Reverse word of string:"<<endl;
+         cout<<str<<endl;
+    
+return 0;
+}
